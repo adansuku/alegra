4d9ba939056a4a0d5406b666456a552383456142
Adan <adangrx@gmail.com>
2020-04-26 21:56:14 +0100

    Update edit title

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -16,28 +16,28 @@
   <div class="row">
 
     <div class="col-lg-3 my-2">
-      <a href="/workers" class="btn btn-sq-lg btn-light p-5 w-100">
+      <a href="/workers" class="btn btn-sq-lg btn-dark p-5 w-100">
         <i class="fas fa-user fa-2x"></i><br />
         <p class="pt-3"> Lista <br>trabajadoras</p>
       </a>
     </div>
 
     <div class="col-lg-3 my-2">
-      <a href="/workers/create" class="btn btn-sq-lg btn-light p-5 w-100">
+      <a href="/workers/create" class="btn btn-sq-lg btn-dark p-5 w-100">
         <i class="fas fa-user-plus fa-2x"></i></i><br />
         <p class="pt-3">Añadir <br> trabajadora</p>
       </a>
     </div>
 
     <div class="col-lg-3 my-2">
-      <a href="/patients" class="btn btn-sq-lg btn-light p-5 w-100">
+      <a href="/patients" class="btn btn-sq-lg btn-dark p-5 w-100">
         <i class="fas fa-users fa-2x"></i><br />
         <p class="pt-3">Lista personas atendidas</p>
       </a>
     </div>
 
     <div class="col-lg-3 my-2">
-      <a href="/patients/create" class="btn btn-sq-lg btn-light p-5 w-100">
+      <a href="/patients/create" class="btn btn-sq-lg btn-dark text-white p-5 w-100">
         <i class="fas fa-users fa-2x"></i> <i class="fas fa-plus-square"></i><br />
         <p class="pt-3">Añadir persona atendida</p>
       </a>
